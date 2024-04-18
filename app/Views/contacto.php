2c9aa286b07907904c50a2738d550abe6c3e99a1
Enzo Fabián López <enzolopez789@gmail.com>
2024-04-18 12:18:08 -0300

    commit

--- app/Views/contacto.php
+++ app/Views/contacto.php
@@ -1,17 +1,23 @@
 <div class="container">
-    <div class="row border border-primary">Logo</div>
+    <div class="row custom-row">
+        <img src="assets\img\contactofondo.png" class="img-fluid">
+    </div>
 </div>
+
+
+
+
 
 <div class="container">
     <div class="row">
-        <div class="col-12 col-md-6 info-contacto border border-danger">
+        <div class="col-12 col-md-6 info-contacto bg-danger text-white p-3">
             <p class="fs-4"><i class="bi bi-geo-alt"></i> Corrientes Capital</p>
             <p class="fs-4"><i class="bi bi-house-door"></i> B° San Martin</p>
             <p class="fs-4"><i class="bi bi-whatsapp"></i> 3794-112233</p>
             <p class="fs-4"><i class="bi bi-instagram"></i> gamershopsrl@gmail.com</p>
         </div>
 
-        <div class="col-12 col-md-6 descripcion-contacto border border-black">
+        <div class="col-12 col-md-6 descripcion-contacto bg-black text-white p-3">
             <p class="fs-6 fst-italic">
                 Para envios a domicilio dentro de la ciudad tenés que contacterte vía <strong>wahtsapp</strong> para coordinar el precio del envío. No dudes en consultarnos en redes sociales o via correo electronico.
             </p>
@@ -19,10 +25,28 @@
     </div>
 </div>
 
-<div class="container">
-    <div class="row border border-primary">ubicacion</div>
-    <div class="row">
-        <div class="col border border-primary">LINK DE IMG AL MAPS</div>
-        <div class="col border border-black">CARD FORMULARIO PARA CONTACTARSE</div>
+<!--FORMULARIO DE CONTACTO-->
+
+<div class="container centered-form">
+    <div class="form-container bg-warning h-100">
+        <div class="container">
+            <h2>Contactános</h2>
+            <form>
+                <div class="form-group">
+                    <label for="nombre">Nombre:</label>
+                    <input type="text" class="form-control" id="nombre" placeholder="Ingrese su nombre" required>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" class="form-control" id="email" placeholder="Ingrese su email" required>
+                </div>
+                <div class="form-group">
+                    <label for="mensaje">Mensaje:</label>
+                    <textarea class="form-control" id="mensaje" rows="3" placeholder="Ingrese su mensaje"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Enviar</button>
+                <button type="reset" class="btn btn-danger">Borrar</button>
+            </form>
+        </div>
     </div>
 </div>

ad99b6e608007d35e4a3bc51980eb1afe3fc8a48
Enzo Fabián López <enzolopez789@gmail.com>
2024-04-20 14:46:34 -0300

    commit

--- app/Views/comercializacion.php
+++ app/Views/comercializacion.php
@@ -7,10 +7,6 @@
 </div>
 
 
-<div class="row mt-3 text-center justify-content-center">
-  <h2>Información</h2>
-</div>
-
 <!--COMO COMPRAR-->
 <div class="container">
   <div class="row justify-content-center m-2">
@@ -60,35 +56,87 @@
 
 <div class="accordion" id="accordionExample">
   <div class="row justify-content-center m-2">
+
     <div class="col-md-8">
-      <div class="accordion-item">
+      <div class="accordion-item"><!--REALIZAR PEDIDO-->
         <h2 class="accordion-header">
           <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
             <p class="card-text">Realizar un pedido</p>
           </button>
         </h2>
-        <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+        <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
           <div class="accordion-body">
             <strong>¿Comò realizar el pedido?</strong>
             <p class="card-text">Solo tenés que seleccionar todos los productos que deseas adquirir. Seguidamente, en el carrito de compras, para conocer el costo del envío colocás tu código postal en el recuadro correspondiente, elegís la mensajería de tu preferencia y debajo seleccionas la forma de pago. Luego hacés clic en el botón COMPRAR y podés acceder como cliente (si ya tenés cuenta en Compra Gamer) o crear un cliente nuevo. Por último, completás los pasos brindados por el asistente, hasta confirmar la compra. Se te asignará un número de pedido y se mostrarán los datos del mismo. También enviaremos un mail a tu correo electrónico registrado con los detalles del pedido realizado.</p>
           </div>
         </div>
       </div>
-      <div class="accordion-item">
+      <div class="accordion-item"><!--PRECIO-->
         <h2 class="accordion-header">
           <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
             <p class="card-text">Precio</p>
           </button>
         </h2>
-        <div id="collapseTwo" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+        <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
           <div class="accordion-body">
             <strong>¿El precio que figura en la web es el precio final?</strong>
             <p class="card-text">Todos los precios en la web incluyen el IVA, y se encuentran expresados en pesos argentinos.</p>
           </div>
         </div>
       </div>
+      <div class="accordion-item"><!--FORMAS DE PAGO-->
+        <h2 class="accordion-header">
+          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+            <p class="card-text">Formas de Pago</p>
+          </button>
+        </h2>
+        <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+          <div class="accordion-body">
+            <strong>¿Cuáles son las formas de pago?</strong>
+            <p class="card-text">Contamos con dos formas de pago: a través de depósito/transferencia bancaria, con la cual obtenés el precio especial, o bien, a través de los métodos Pago Gamer (Visa o MasterCard) o MercadoPago (Tarjetas online, PagoFácil y RapiPago) con los cuales podés abonar en cuotas, al precio de lista.</p>
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item"><!--DEPOSITO TRANSFERENCIA BANCARIA-->
+        <h2 class="accordion-header">
+          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
+            <p class="card-text">Deposito transferencia bancaria</p>
+          </button>
+        </h2>
+        <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+          <div class="accordion-body">
+            <strong>¿Cómo abono a través de depósito/transferencia?</strong>
+            <p class="card-text">Una vez se realiza el pedido, te facilitamos los datos del CBU. Debes abonar e informar el pago desde nuestra web, antes de la fecha de vencimiento de la reserva.</p>
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item"><!--ENVIOS-->
+        <h2 class="accordion-header">
+          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="true" aria-controls="collapseFive">
+            <p class="card-text">Enviós</p>
+          </button>
+        </h2>
+        <div id="collapseFive" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+          <div class="accordion-body">
+            <strong>¿Cómo gestiono el envío de mi pedido?</strong>
+            <p class="card-text">En primer lugar, para conocer el costo del envío, una vez al agregues al carrito tu compra, solo debes colocar tu código postal en el recuadro correspondiente, seleccionar la mensajería de tu preferencia y elegir si deseas el retiro en alguna sucursal o la entrega a domicilio. Actualmente realizamos envíos a todo el país través de Oca; y si te encontrás en CABA o alrededores, podrás seleccionar el servicio de Mensajería Privada que es exclusivo de Compra Gamer. Tené en cuenta que, para calcular el costo del envío, se toman en consideración tanto las dimensiones y peso del paquete como la distancia de la localidad de entrega.</p>
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item"><!--FACTURACION-->
+        <h2 class="accordion-header">
+          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="true" aria-controls="collapseSix">
+            <p class="card-text">Facturación</p>
+          </button>
+        </h2>
+        <div id="collapseSix" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+          <div class="accordion-body">
+            <strong>¿Cómo tramito la factura de mi compra?</strong>
+            <p class="card-text">En todas las compras efectuadas en la web, brindamos sin excepción alguna, la factura de compra. Una vez que realiza y abona el pedido, enviamos a tu dirección de correo electrónico la factura correspondiente. Por supuesto, también podés descargarla desde la sección Mi cuenta < Mis facturas. En caso de que precises factura A, solo debes ingresar tu CUIT al cargar el pedido por la web. Tené en cuenta que la factura A puede tener percepciones.</p>
+          </div>
+        </div>
+      </div>
     </div>
-
 
   </div>
 </div>
